d744c0ca2581d58ede22735fb0dfdb84b660b841
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-11-12 19:14:25 -0300

    Condição

---
+++ Condições/ex008.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60)
+{
+    console.log("Você ultrpassou a velocidade permitida")
+}
+console.log("Dirija sempre usando cinto  de segurança!")
